ba3432887f1a1980c1fda53448e8d4ad8de19eb0
leandro <leandro@leandro-H110M-H>
2022-05-31 22:33:03 -0300

    Creation of project and classes

--- src/cajeroAutomatico/CajeroAutomatico.java
+++ src/cajeroAutomatico/CajeroAutomatico.java
@@ -1,10 +1,86 @@
 package cajeroAutomatico;
+import java.util.Scanner;
+
+
+
+
 
 public class CajeroAutomatico {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		UsuarioCajeroAutomatico vectorDePersonas[] = new UsuarioCajeroAutomatico[1];
 
+		Scanner teclado = new Scanner(System.in);
+		String entrarAlMenu="";
+		boolean salirDelMenu = false;
+		boolean validarDatos = false;
+		
+		
+		System.out.println("--------------------------------------------\n");
+		System.out.println("Bienvenido al Simulador de Cajero Automatico\n");
+		System.out.println("Crear 5 usuarios con nombre, apellido y saldo en cuenta:\n");
+		System.out.println("--------------------------------------------");
+		
+		//ACA COMIENZA EL ALTA DE LOS USUARIOS
+		//HERE BEGINS THE REGISTRATION OF THE USERS
+		
+		for (int i = 0; i < vectorDePersonas.length; i++) {
+			
+			System.out.println("*Ingrese el nombre de la persona:" + i);
+			
+			String nombreTemporal = teclado.next();
+			
+			System.out.println("*Ingrese el apellido de la persona:" + i);
+			
+			String apellidoTemporal = teclado.next();
+			
+			System.out.println("*Ingrese el saldo de la persona:" + i);
+			
+			double saldoTemporal = teclado.nextInt();
+			
+			//EN LA LINEA 42 SE CREAN LOS USUARIOS INVOCANDO AL CONSTRUCTOR QUE TOMA 3 PARAMETROS
+			//LINE 42 USERS ARE CREATED USING THE CONSTRUCTOR THAT TAKES THREE PARAMETERS
+			
+			vectorDePersonas[i]=new UsuarioCajeroAutomatico(nombreTemporal, apellidoTemporal, saldoTemporal);
+			
+			}
+		
+		do {
+			System.out.println("--------------------------------------------\n");
+			System.out.println("Seleccione la opcion del menu:\n");
+			
+			System.out.println("a) DEPOSITAR");
+			
+			System.out.println("b) EXTRAER");
+			
+			System.out.println("c) CONSULTAR");
+			
+			System.out.println("d) SALIR");
+			
+			System.out.println("--------------------------------------------\n");
+			
+			entrarAlMenu = teclado.next();
+			
+			
+			switch(entrarAlMenu) {
+			
+			case "a":
+				break;
+			case "b":
+				break;
+			case "c":
+				break;
+			case "d":
+				break;
+			default:
+				
+			
+			}
+		}
+		while(salirDelMenu = false);
+		
+		
 	}
 
 }
--- src/cajeroAutomatico/UsuarioCajeroAutomatico.java
+++ src/cajeroAutomatico/UsuarioCajeroAutomatico.java
@@ -4,4 +4,36 @@ public class UsuarioCajeroAutomatico {
 	
 	private String nombre;
 	private String apellido;
+	private double saldo;
+	
+	
+	
+	public UsuarioCajeroAutomatico(String nombre, String apellido, double saldo) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.saldo = saldo;
+	}
+	public UsuarioCajeroAutomatico() {
+		
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	
 }
