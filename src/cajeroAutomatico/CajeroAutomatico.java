4a63577e008ebc738974719687457a1a23f4b1f0
leandro <leandro@leandro-H110M-H>
2022-06-05 20:02:05 -0300

    Implementation of iterations to run the program

--- src/cajeroAutomatico/CajeroAutomatico.java
+++ src/cajeroAutomatico/CajeroAutomatico.java
@@ -1,21 +1,24 @@
 package cajeroAutomatico;
 import java.util.Scanner;
 
+public class CajeroAutomatico 
 
+{
 
-
-
-public class CajeroAutomatico {
-
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	
+	{
 		
-		UsuarioCajeroAutomatico vectorDePersonas[] = new UsuarioCajeroAutomatico[1];
+		UsuarioCajeroAutomatico vectorDePersonas[] = new UsuarioCajeroAutomatico[2];
 
 		Scanner teclado = new Scanner(System.in);
 		String entrarAlMenu="";
-		boolean salirDelMenu = false;
+		boolean salirDelMenu = true;
+		boolean salirDelPrograma = true;
 		boolean validarDatos = false;
-		
+		boolean salirDelMenuSalir = true;
+		UsuarioCajeroAutomatico usuarioActivo = null;
+		double totalidadDeSaldo = 0.0;
 		
 		System.out.println("--------------------------------------------\n");
 		System.out.println("Bienvenido al Simulador de Cajero Automatico\n");
@@ -35,6 +38,10 @@ public class CajeroAutomatico {
 			
 			String apellidoTemporal = teclado.next();
 			
+			System.out.println("*Ingrese el dni de la persona:" + i);
+			
+			int dniTemporal = teclado.nextInt();
+			
 			System.out.println("*Ingrese el saldo de la persona:" + i);
 			
 			double saldoTemporal = teclado.nextInt();
@@ -42,12 +49,58 @@ public class CajeroAutomatico {
 			//EN LA LINEA 42 SE CREAN LOS USUARIOS INVOCANDO AL CONSTRUCTOR QUE TOMA 3 PARAMETROS
 			//LINE 42 USERS ARE CREATED USING THE CONSTRUCTOR THAT TAKES THREE PARAMETERS
 			
-			vectorDePersonas[i]=new UsuarioCajeroAutomatico(nombreTemporal, apellidoTemporal, saldoTemporal);
+			vectorDePersonas[i]=new UsuarioCajeroAutomatico(nombreTemporal, apellidoTemporal, dniTemporal, saldoTemporal);
 			
 			}
+		do {
+			
 		
 		do {
 			System.out.println("--------------------------------------------\n");
+			System.out.println("*Ingrese el dni de la persona para operar:");
+			System.out.println("--------------------------------------------\n");
+			
+			int dniTemporal = teclado.nextInt(); 
+				
+				for(int i= 0; i < vectorDePersonas.length; i++) 
+				{
+					if(dniTemporal == vectorDePersonas[i].getDni()) 
+						{
+						
+						usuarioActivo = vectorDePersonas[i]; 
+						
+						salirDelMenu = false; 
+						}	
+				}
+				
+				if(usuarioActivo != null) 
+				
+				{
+					
+					if(dniTemporal == usuarioActivo.getDni()) 
+					
+					{
+						System.out.println("BIENVENIDO AL CAJERO AUTOMATICO");
+						System.out.println(usuarioActivo.getNombre());
+						System.out.println(usuarioActivo.getApellido());
+						System.out.println(usuarioActivo.getDni());
+						
+					}
+				
+				}
+				
+				else
+					
+					{
+						System.out.println("DNI NO ENCONTRADO, INGRESE UN DNI CORRECTO");
+					}
+				
+			}while(salirDelMenu == true);
+		
+		do {
+			
+			System.out.println("--------------------------------------------\n");
+			
 			System.out.println("Seleccione la opcion del menu:\n");
 			
 			System.out.println("a) DEPOSITAR");
@@ -63,24 +116,137 @@ public class CajeroAutomatico {
 			entrarAlMenu = teclado.next();
 			
 			
-			switch(entrarAlMenu) {
+			switch(entrarAlMenu) 
 			
-			case "a":
-				break;
-			case "b":
-				break;
-			case "c":
-				break;
-			case "d":
-				break;
-			default:
-				
+			{
 			
-			}
-		}
-		while(salirDelMenu = false);
-		
-		
-	}
+				case "a":
+					
+					System.out.println("MONTO A DEPOSITAR:");
+					
+					double saldoACargar = teclado.nextDouble();
+					
+					totalidadDeSaldo = usuarioActivo.getSaldo() + saldoACargar;
+					
+					usuarioActivo.setSaldo(totalidadDeSaldo);
+					
+					salirDelMenu = true;
+					
+					break;
+					
+				case "b":
+					
+					boolean salirDelWhile = true;
+					
+					while(salirDelWhile == true) 
+						
+					{
+						System.out.println("MONTO A EXTRAER:");
+						
+						double saldoARetirar = teclado.nextDouble();
+						
+						if(saldoARetirar <= usuarioActivo.getSaldo()) 
+						
+							{
+								System.out.println("HA RETIRADO EXITOSAMENTE");
+								totalidadDeSaldo = usuarioActivo.getSaldo() - saldoARetirar;
+								
+								usuarioActivo.setSaldo(totalidadDeSaldo);
+								
+								salirDelWhile = false;
+								
+								salirDelMenu = true;
+								
+							}
+						
+						else 
+							
+							{
+								System.out.println("NO POSEE SALDO SUFICIENTE");
+								salirDelWhile = true;
+							}
+						
+					}
+					
+					break;
+					
+				case "c":
+					
+					System.out.println("SU SALDO ES:");
+					
+					System.out.println(usuarioActivo.getSaldo());
+					
+					salirDelMenu = true;
+					
+					break;
+					
+				case "d":
+					do {
+						System.out.println("--------------------------------------------\n");
+						System.out.println("ELIJA LA OPCION PARA PROSEGUIR:");
+						System.out.println("1) OPERAR CON OTRA CUENTA");
+						System.out.println("2) SALIR DEL PROGRAMA");
+						System.out.println("--------------------------------------------\n");
+						
+						int salirDelProgramaFinal = teclado.nextInt();
+						
+						switch(salirDelProgramaFinal) {
+						
+						case 1:
+							
+							salirDelMenu = false;
+							
+							salirDelPrograma = true;
+							
+							salirDelMenuSalir = false;
+							
+							break;
+							
+						case 2:
+							
+							System.out.println("HA SALIDO EXITOSAMENTE DEL PROGRAMA");
+							
+							salirDelMenu = false;
+							
+							salirDelPrograma = false;
+							
+							salirDelMenuSalir = false;
+							
+							break;
+							
+						default:
+							
+							System.out.println("--------------------------------------------");
+							
+							System.out.println("CARACTER INCORRECTO, INGRESE UN CARACTER VALIDO");
+							
+							System.out.println("--------------------------------------------\n");
+							
+							salirDelMenuSalir = true;
+							
+						}
+						
+					}while(salirDelMenuSalir == true);
 
+					break;
+					
+				default:
+					
+					System.out.println("--------------------------------------------");
+					
+					System.out.println("CARACTER INCORRECTO, INGRESE UN CARACTER VALIDO");
+					
+					System.out.println("--------------------------------------------\n");
+					
+					salirDelMenu = true;
+					
+				}
+			
+		    }
+		
+		while(salirDelMenu == true);
+		
+		}while(salirDelPrograma==true);
+	}
+	
 }
--- src/cajeroAutomatico/UsuarioCajeroAutomatico.java
+++ src/cajeroAutomatico/UsuarioCajeroAutomatico.java
@@ -4,13 +4,15 @@ public class UsuarioCajeroAutomatico {
 	
 	private String nombre;
 	private String apellido;
+	private int dni;
 	private double saldo;
 	
 	
 	
-	public UsuarioCajeroAutomatico(String nombre, String apellido, double saldo) {
+	public UsuarioCajeroAutomatico(String nombre, String apellido,int dni, double saldo) {
 		this.nombre = nombre;
 		this.apellido = apellido;
+		this.dni = dni;
 		this.saldo = saldo;
 	}
 	public UsuarioCajeroAutomatico() {
@@ -27,6 +29,12 @@ public class UsuarioCajeroAutomatico {
 	}
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
+	}
+	public int getDni() {
+		return dni;
+	}
+	public void setDni(int dni) {
+		this.dni = dni;
 	}
 	public double getSaldo() {
 		return saldo;
